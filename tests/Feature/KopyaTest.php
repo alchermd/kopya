1f6f5f95a4e02914f11cc1bc75c8435ca1990d27
John Alcher M. Doloiras <johnalcherdoloiras@gmail.com>
2018-06-29 10:14:11 +0800

    Test that an authenticated user can access the dashboard

--- tests/Feature/KopyaTest.php
+++ tests/Feature/KopyaTest.php
@@ -5,14 +5,28 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class KopyaTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function guest_cant_access_dashboard()
     {
         $response = $this->get('/home');
 
         $response->assertStatus(302);
+    }
+
+    /** @test */
+    public function auth_user_can_access_dashboard()
+    {
+        $user = factory(\App\User::class)->create();
+
+        $response = $this->actingAs($user)
+                         ->get('/home');
+
+        $response->assertStatus(200);
     }
 }
