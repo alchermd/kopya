b5598d0834eeeef0e8d3bb4f248d38d0c2226cc2
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2018-07-04 13:22:47 +0800

    Update testcase to use `make` on factory
    Also use `RefreshDatabase` instead of `DatabaseMigrations`

--- tests/Feature/KopyaTest.php
+++ tests/Feature/KopyaTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,21 +10,18 @@ class KopyaTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function guest_cant_create_new_kopyas()
-    {
-        $reponse = $this->json('POST', '/kopyas');
-
-        $reponse->assertStatus(401);
-    }
-
-    /** @test */
     public function user_can_create_new_kopyas()
     {
         $user = factory(\App\User::class)->create();
         $kopya = factory(\App\Kopya::class)->make(['user_id' => $user->id]);
 
         $response = $this->actingAs($user)
-                         ->json('POST', '/kopyas', $kopya->toArray());
+                         ->get('/kopyas/create');
+
+        $response->assertSee('New Kopya');
+
+        $response = $this->actingAs($user)
+                         ->post('/kopyas', $kopya->toArray());
 
         $response->assertStatus(302);
         $this->assertDatabaseHas('kopyas', $kopya->toArray());
