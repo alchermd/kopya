14538efc414191bbe0f9b169549dce042bac0e19
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2018-07-05 17:54:46 +0800

    Test that a user can browse kopyas

--- tests/Feature/KopyaTest.php
+++ tests/Feature/KopyaTest.php
@@ -26,4 +26,20 @@ class KopyaTest extends TestCase
         $response->assertStatus(302);
         $this->assertDatabaseHas('kopyas', $kopya->toArray());
     }
+
+    /** @test */
+    public function user_can_browse_kopyas()
+    {
+        $user = factory(\App\User::class)->create();
+        $kopyas = factory(\App\Kopya::class, 5)->create();
+
+        $response = $this->actingAs($user)
+                         ->get('/kopyas');
+
+        $response->assertSee('Browse');
+
+        $kopyas->each(function ($kopya) use ($response) {
+            $response->assertSee($kopya->title);
+        });
+    }
 }
